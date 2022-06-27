7513d4f7fcae874329a9bc0b9ac2de8a5549269d
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-27 22:53:05 +0300

    Initial Commit

---
+++ TableViewStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
